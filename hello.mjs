fab1590ed568a36f6af6e2407eab65ee3d9d58f1
Muhammad Adnan Khan <98257145+Mr-Adnankhan@users.noreply.github.com>
2023-11-25 12:53:54 +0500

    for loops

---
+++ hello.mjs
@@ -0,0 +1,17 @@
+let students = [
+  "Adnan ",
+  "khan ",
+  "abdul haq",
+  "sheikh ",
+  "Asad ",
+  "ibrahim",
+  "Muhammad Dawood Khan",
+];
+
+for (let a = 0; a < students.length; a++) {
+  if (students[a].startsWith("A") || students[a].startsWith("a")) {
+    console.log(`Mr. ${students[a]} invited to taufeeq wedding ceremony`);
+  } else {
+    console.log(no);
+  }
+}
